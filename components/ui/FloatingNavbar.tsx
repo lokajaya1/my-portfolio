3ac8292c96ec8b984659c2510eafe492e847d2ea
lokajaya1 <lokajayandala1@gmail.com>
2024-06-04 04:29:38 +0700

    update bg navbar

--- components/ui/FloatingNavbar.tsx
+++ components/ui/FloatingNavbar.tsx
@@ -49,7 +49,7 @@ export const FloatingNav = ({
           )}
           style={{
             backdropFilter: "blur(16px) saturate(180%)",
-            backgroundColor: "rgba(17, 25, 40, 0.25)",
+            backgroundColor: "rgba(0, 0, 0, 0.9)",
             borderRadius: "12px",
             border: "1px solid rgba(255, 255, 255, 0.125)",
           }}
